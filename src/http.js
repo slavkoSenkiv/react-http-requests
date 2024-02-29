685e94e9f5f94061ea883a5b2002caaea77aa86a
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-02-29 19:16:40 +0100

    add user places changes

--- src/http.js
+++ src/http.js
@@ -8,9 +8,19 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
+export async function fetchUserPlaces() {
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch user places");
+  }
+
+  return resData.places;
+}
 
 export async function updateUserPlaces(places) {
-  const response = await fetch("http://localhost:3000/user-placess", {
+  const response = await fetch("http://localhost:3000/user-places", {
     method: "PUT",
     body: JSON.stringify({places: places}), //{places} would also work
     headers: {
